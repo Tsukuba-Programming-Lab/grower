dce3eb685cabcbe429b631d2732ce09bfe09e47c
itsu-dev <itsu020402@gmail.com>
2025-08-09 21:51:11 +0900

    feat: initialize

---
+++ README.md
@@ -0,0 +1,12 @@
+# Grower
+ブラウザ上で稼働する言語ランタイムのためのライブラリ（**G**eneral **R**untime **O**n **WE**b browser**R**s）
+
+## Structures
+
+### grower-rs
+
+言語ランタイムのうち，WebAssembly コードとして動作する部分を実装するための Rust クレート．
+
+### grower-js
+
+言語ランタイムのうち，JavaScript コードとして動作する部分を実装するための Rust クレート．
---
+++ grower-rs/crates/jsni/src/lib.rs
@@ -0,0 +1,184 @@
+use wasm_bindgen::prelude::wasm_bindgen;
+use std::vec;
+
+#[allow(unused_imports)]
+use wasm_bindgen::JsValue;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum JSNIKind {
+    I8,
+    I16,
+    I32,
+    I64,
+    U8,
+    U16,
+    U32,
+    U64,
+    F32,
+    F64,
+    String,
+    U8Array,
+    Reference,
+    Null,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct JSNIValue {
+    kind: JSNIKind,
+    value: u64,
+}
+
+macro_rules! impl_from_primitive {
+    ($ty:ty, $kind:expr) => {
+        impl From<$ty> for JSNIValue {
+            fn from(value: $ty) -> Self {
+                JSNIValue {
+                    kind: $kind,
+                    value: value as u64,
+                }
+            }
+        }
+    };
+}
+
+impl_from_primitive!(i8, JSNIKind::I8);
+impl_from_primitive!(i16, JSNIKind::I16);
+impl_from_primitive!(i32, JSNIKind::I32);
+impl_from_primitive!(i64, JSNIKind::I64);
+impl_from_primitive!(u8, JSNIKind::U8);
+impl_from_primitive!(u16, JSNIKind::U16);
+impl_from_primitive!(u32, JSNIKind::U32);
+impl_from_primitive!(u64, JSNIKind::U64);
+impl_from_primitive!(usize, JSNIKind::Reference);
+
+impl From<f32> for JSNIValue {
+    fn from(value: f32) -> Self {
+        let bytes = value.to_le_bytes();
+        JSNIValue {
+            kind: JSNIKind::F32,
+            value: ((bytes[0] as u64) << 0)
+                | ((bytes[1] as u64) << 8)
+                | ((bytes[2] as u64) << 16)
+                | ((bytes[3] as u64) << 24),
+        }
+    }
+}
+
+impl From<f64> for JSNIValue {
+    fn from(value: f64) -> Self {
+        let bytes = value.to_le_bytes();
+        JSNIValue {
+            kind: JSNIKind::F64,
+            value: ((bytes[0] as u64) << 0)
+                | ((bytes[1] as u64) << 8)
+                | ((bytes[2] as u64) << 16)
+                | ((bytes[3] as u64) << 24)
+                | ((bytes[4] as u64) << 32)
+                | ((bytes[5] as u64) << 40)
+                | ((bytes[6] as u64) << 48)
+                | ((bytes[7] as u64) << 56),
+        }
+    }
+}
+
+impl From<Vec<u8>> for JSNIValue {
+    fn from(value: Vec<u8>) -> Self {
+        let len = value.len();
+        let ptr = value.as_ptr() as *mut u8;
+        std::mem::forget(value); // Prevent Rust from freeing the memory
+        JSNIValue {
+            kind: JSNIKind::U8Array,
+            // high: len 32bit, low: ptr 64bit
+            value: (len as u64) << 32 | ptr as u64,
+        }
+    }
+}
+
+impl From<String> for JSNIValue {
+    fn from(value: String) -> Self {
+        let len = value.len();
+        let bytes = value.into_bytes();
+        let ptr = bytes.as_ptr() as *mut u8;
+        std::mem::forget(bytes); // Prevent Rust from freeing the memory
+        JSNIValue {
+            kind: JSNIKind::String,
+            value: (len as u64) << 32 | ptr as u64,
+        }
+    }
+}
+
+impl JSNIValue {
+    pub fn null() -> Self {
+        JSNIValue {
+            kind: JSNIKind::Null,
+            value: 0,
+        }
+    }
+}
+
+pub struct JavaScriptNativeInterface {
+}
+
+#[wasm_bindgen]
+extern "C" {
+    async fn jsni_call(js_func_name: *const u8, args: *const u8, args_count: usize) -> JsValue;
+}
+
+fn vec_onto_box<T>(vec: Vec<T>) -> *mut Vec<T> {
+    Box::into_raw(Box::new(vec))
+}
+
+/// Allocates a JSNIValue array in the heap and returns a fat pointer.
+#[wasm_bindgen]
+pub fn alloc_jsni_value(size: usize) -> u64 {
+    let mut vec = vec![JSNIValue::null(); size];
+    let ptr = vec.as_mut_ptr() as *mut u8;
+    let vec_ptr = vec_onto_box(vec);
+    (vec_ptr as u64) << 32 | ptr as u64
+}
+
+/// Deallocates a JSNIValue array allocated by `alloc_jsni_value.
+#[wasm_bindgen]
+pub fn alloc(size: usize) -> u64 {
+    let mut vec = vec![0u8; size];
+    let ptr = vec.as_mut_ptr() as *mut u8;
+    let vec_ptr = vec_onto_box(vec);
+    (vec_ptr as u64) << 32 | ptr as u64
+}
+
+impl JavaScriptNativeInterface {
+    pub fn new() -> Self {
+        JavaScriptNativeInterface {}
+    }
+
+    /// Calls the JavaScript function.
+    /// Must be set registers with uarguments to pass to the JavaScript function before calling this function.
+    /// Returns a vector of results.
+    /// The first register is the count of results, followed by the results themselves.
+    pub async fn call(&mut self, js_func_name: String, args: Vec<JSNIValue>) -> Vec<JSNIValue> {
+        let js_func_name = JSNIValue::from(js_func_name);
+        let js_func_name_ptr = &js_func_name as *const JSNIValue as *const u8;
+        
+        let return_values_ptr_raw = jsni_call(js_func_name_ptr, args.as_ptr() as *mut u8, args.len()).await;
+        let return_values = unsafe { Box::from_raw(return_values_ptr_raw.as_f64().unwrap() as u64 as *mut Vec<JSNIValue>) };
+
+        // Deallocate the arguments
+        for arg in args.iter() {
+            match arg.kind {
+                JSNIKind::U8Array => {
+                    let len = (arg.value >> 32) as usize;
+                    let ptr = (arg.value & 0xFFFFFFFF) as *mut u8;
+                    unsafe { Vec::from_raw_parts(ptr, len, len) };
+                }
+                JSNIKind::String => {
+                    let len = (arg.value >> 32) as usize;
+                    let ptr = (arg.value & 0xFFFFFFFF) as *mut u8;
+                    unsafe { String::from_raw_parts(ptr, len, len) };
+                }
+                _ => {}
+            }
+        }
+
+        *return_values
+    }
+}
